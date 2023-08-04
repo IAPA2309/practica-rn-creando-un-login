77a3f8ef927f2d9b714f041f1c864a3fc1c658e3
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-04 11:36:46 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+// import { TextInput } from 'react-native-web';
+import axios from 'axios';
 
 export default function App() {
+  const [user, onChangeUser] = React.useState("user");
+  const [password, onChangePassword] = React.useState("password");
+
+  function loginUser() {
+    axios
+      .post(
+        "http://localhost:5000/login",
+        {
+          username: user,
+          password: password,
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        styles={styles.input}
+        onChangeText={onChangeUser}
+        value={user}
+        placeholder="usuario"
+      />
+      <TextInput
+        styles={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+        placeholder="contraseña"
+      />
+      <Button 
+      title="Iniciar Sesion"
+      onPress={loginUser}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 10,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
